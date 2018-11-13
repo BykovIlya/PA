cd5f23de37d723f74eb925f89f45c670dd64231b
BykovIlya <bikovbiv@gmail.com>
2018-11-14 00:59:31 +0300

    delete obj

---
+++ LAB1/cmake-build-debug/CMakeFiles/LAB1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB1.dir/first.c.o"
+  "CMakeFiles/LAB1.dir/second.c.o"
+  "CMakeFiles/LAB1.dir/third.c.o"
+  "libLAB1.pdb"
+  "libLAB1.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LAB1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAB1/cmake-build-debug/CMakeFiles/LAB1.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libLAB1.a"
+)
